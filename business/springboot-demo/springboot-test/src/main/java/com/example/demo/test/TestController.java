40561797a64426ec2c77effacaaff7c1915af137
@zj <593625354@qq.com>
2019-08-08 17:59:55 +0800

    fisrt commit

---
+++ business/springboot-demo/springboot-test/src/main/java/com/example/demo/test/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangjie
+ * @date 2019/8/8 17:26
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "This is a test!";
+    }
+}
